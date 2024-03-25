046db829817433d78bfae3d54e7f84da80bb11f8
avbravo <avbravo@avbravo-ThinkCentre-M70t-Gen-3>
2024-03-25 09:14:14 -0500

    1.1

--- src/main/java/com/accreditation/configuration/ApplicationScheduler.java
+++ src/main/java/com/accreditation/configuration/ApplicationScheduler.java
@@ -89,9 +89,11 @@ public class ApplicationScheduler implements Serializable, JmoordbCoreXHTMLUtil 
     }
 // </editor-fold>
 
-    // <editor-fold defaultstate="collapsed" desc="void verify()">
+    // <editor-fold defaultstate="collapsed" desc="void schedule()">
     
-    @Schedule(second = "30", minute = "25", hour = "8", persistent = false)
+//    @Schedule(second = "30", minute = "25", hour = "8", persistent = false)
+//    @Schedule(second = "30", minute = "30", hour = "10", persistent = false)
+    @Schedule(second = "30", minute = "50", hour = "9", persistent = false)
   
   
 
